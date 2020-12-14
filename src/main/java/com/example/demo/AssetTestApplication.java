aaf80c1518d4fb03d991f14579f480b33ae87a61
GDM94 <giovanni.dimartino1994@gmail.com>
2020-12-14 09:59:55 +0100

    init asset test

---
+++ src/main/java/com/example/demo/AssetTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetTestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AssetTestApplication.class, args);
+	}
+
+}
