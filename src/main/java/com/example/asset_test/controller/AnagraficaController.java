b14af4ebfd7ffe500f08ed2d2d9ce49913562759
GDM94 <giovanni.dimartino1994@gmail.com>
2020-12-15 14:09:31 +0100

    anagrafica

--- src/main/java/com/example/asset_test/controller/AnagraficaController.java
+++ src/main/java/com/example/asset_test/controller/AnagraficaController.java
@@ -5,9 +5,8 @@ import com.example.asset_test.service.impl.AnagraficaServiceImpl;
 import com.example.communication.model.Anagrafica;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -24,5 +23,27 @@ public class AnagraficaController {
     public LinkedHashMap getAllanagrafica() throws JSONException {
         return anagraficaService.getAllanagrafica();
     }
+
+    @GetMapping("/{id}")    // GET Method for reading operation
+    public LinkedHashMap getAnagraficaById(@PathVariable(value = "id") Long anaId) throws JSONException {
+        return anagraficaService.anagraficaById(anaId);
+    }
+
+    @PostMapping   // GET Method for reading operation
+    public LinkedHashMap newAnagrafica(@RequestBody Anagrafica ana) throws JSONException {
+        return anagraficaService.newAnagrafica(ana.getIdana(), ana.getNome(), ana.getCognome());
+    }
+
+    @PutMapping("/{id}")
+    public LinkedHashMap updateAnagrafica(@PathVariable(value = "id") long anaId,  @RequestBody Anagrafica anaDetails) throws JSONException {
+        return anagraficaService.updateAnagrafica(anaId, anaDetails.getNome(), anaDetails.getCognome());
+    }
+
+    @DeleteMapping("/{id}")    // GET Method for reading operation
+    public LinkedHashMap deleteAnagrafica(@PathVariable(value = "id") Long anaId) throws JSONException {
+        return anagraficaService.deleteAnagrafica(anaId);
+    }
+
+
 
 }
--- src/main/java/com/example/asset_test/service/impl/AnagraficaServiceImpl.java
+++ src/main/java/com/example/asset_test/service/impl/AnagraficaServiceImpl.java
@@ -27,7 +27,62 @@ public class AnagraficaServiceImpl {
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         HttpEntity<String> entity = new HttpEntity<>(jo.toString(), headers);
         ResponseEntity<LinkedHashMap> response = restTemplate.postForEntity(urlDB, entity, LinkedHashMap.class);
-        System.out.println(response.getBody());
         return response.getBody();
     }
+
+    public LinkedHashMap newAnagrafica(Long id, String nome, String cognome) throws JSONException {
+        String query = String.format("mutation {newAnagrafica(id: %s, nome: \"%s\", cognome: \"%s\") { nome cognome } }", id.toString(), nome, cognome);
+        jo.put("query", query);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<>(jo.toString(), headers);
+        ResponseEntity<LinkedHashMap> response = restTemplate.postForEntity(urlDB, entity, LinkedHashMap.class);
+        return response.getBody();
+    }
+
+    public LinkedHashMap anagraficaById(Long id) throws JSONException {
+        String query = String.format("query {anagraficaById(id: %s) { nome cognome } }", id.toString());
+        jo.put("query", query);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<>(jo.toString(), headers);
+        ResponseEntity<LinkedHashMap> response = restTemplate.postForEntity(urlDB, entity, LinkedHashMap.class);
+        return response.getBody();
+    }
+
+    public LinkedHashMap updateAnagrafica(Long id, String nome, String cognome) throws JSONException {
+        String query = String.format("mutation {updateAnagrafica(id: %s, nome: \"%s\", cognome: \"%s\") { nome cognome } }", id.toString(), nome, cognome);
+        jo.put("query", query);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<>(jo.toString(), headers);
+        ResponseEntity<LinkedHashMap> response = restTemplate.postForEntity(urlDB, entity, LinkedHashMap.class);
+        return response.getBody();
+    }
+
+    public LinkedHashMap deleteAnagrafica(Long id) throws JSONException {
+        String query = String.format("mutation {deleteAnagrafica(id: %s)}", id.toString());
+        jo.put("query", query);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<>(jo.toString(), headers);
+        ResponseEntity<LinkedHashMap> response = restTemplate.postForEntity(urlDB, entity, LinkedHashMap.class);
+        return response.getBody();
+    }
+
+
+
+
+
+
+
+
 }
